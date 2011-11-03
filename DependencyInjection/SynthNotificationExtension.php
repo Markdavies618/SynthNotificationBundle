b75c79778facbb0b96ada9aeab2c9863f6f521b4
Dom Udall <dom@synthmedia.co.uk>
2011-11-03 14:09:01 +0000

    Adding BSD License info

--- DependencyInjection/SynthNotificationExtension.php
+++ DependencyInjection/SynthNotificationExtension.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of the Synth Notification Bundle.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Dom Udall <dom@synthmedia.co.uk>
+ */
+
 namespace Synth\NotificationBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
